c2587a3f2d4a98eed125d076f17795ac6133a492
dwiki1968 <dwiki1968@gmail.com>
2022-06-26 22:49:51 +0700

    solvedf

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './App.css';
+import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from "./data/contacts.json";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      {contacts.map((contact, i) => {
+        return <Contact key={i} data={contact} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,20 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import "./Contact.css";
+import React from "react";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = (props) => {
+  const { name, phone, photo, email } = props.data;
+  return (
+    <div className="card">
+      <img src={photo} alt="contact-img" />
+      <div className="content">
+        <h2>{name}</h2>
+        <p>{phone}</p>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
